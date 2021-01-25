76704a8dcecb0e601b04258c37a8934a836509ac
Meg <ndrei61999@gmail.com>
2021-01-25 08:36:25 -0800

    Sorting in different ways

--- InsertionSort/invsort.js
+++ InsertionSort/invsort.js
@@ -13,3 +13,28 @@ function invertionSort(arr) {
 
 
 console.log(invertionSort([5,6,7,8,9,1,2,3,4]))
+
+
+
+
+
+/* 
+		Solution with reverse
+*/
+
+function sort(arr) {
+	for (var i = 1; i < arr.length; i++) {
+		let k = arr[i];
+		let j = i - 1;
+		while ((j > -1) && (arr[j] > k)) {
+			arr[j + 1] = arr[j]
+			j--;
+		}
+		arr[j + 1] = k;
+	}
+	return arr;
+}
+let sorted = sort([5,6,7,8,9,1,2,3,4])
+let reversed = sorted.reverse()
+console.log(reversed)
+
